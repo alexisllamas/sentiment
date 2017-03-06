1019694c08577b1a3a51011f46522beba7ef3213
alexisllamas <alexisnavarro23@gmail.com>
2017-03-06 07:01:50 +0000

    heroku conf

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,7 @@ defmodule Etlicus.UserSocket do
   # channel "room:*", Etlicus.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
